41882e75b160525e5ed0e14e2e589a3843d810fa
hejian.home <flystarhe@qq.com>
2024-06-04 01:20:30 +0800

    entrypoint.sh - 默认端口10000

--- fiftyone/entrypoint.sh
+++ fiftyone/entrypoint.sh
@@ -4,13 +4,13 @@ set -e
 MODE="${1:-null}"
 
 if [ "${MODE}" = 'ssh' ]; then
-    /usr/sbin/sshd -p 9000
+    /usr/sbin/sshd -p 10000
 elif [ "${MODE}" = 'tornado' ]; then
-    nohup python /workspace/main.py 9000 ${@:2} >> /workspace/log.out 2>&1 &
+    nohup python /workspace/main.py 10000 ${@:2} >> /workspace/log.out 2>&1 &
 elif [ "${MODE}" = 'fastapi' ]; then
-    nohup uvicorn main:app --host 0.0.0.0 --port 9000 --workers 1 >> /workspace/log.out 2>&1 &
+    nohup uvicorn main:app --host 0.0.0.0 --port 10000 --workers 1 >> /workspace/log.out 2>&1 &
 elif [ "${MODE}" = 'notebook' ]; then
-    nohup jupyter notebook --ip='*' --port=9000 --notebook-dir='/workspace' --NotebookApp.token='hi' --no-browser --allow-root >> /workspace/log.out 2>&1 &
+    nohup jupyter notebook --ip='*' --port=10000 --notebook-dir='/workspace' --NotebookApp.token='hi' --no-browser --allow-root >> /workspace/log.out 2>&1 &
 fi
 
 tail -f /dev/null
